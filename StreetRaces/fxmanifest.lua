ae85f1b7a04755ec92b3227139449763e44a90b7
Khralis <50895160+Khralis@users.noreply.github.com>
2024-11-06 21:37:23 -0500

    Add files via upload

---
+++ StreetRaces/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'khralis'
+description 'streetraces'
+version '0.0.2'
+
+server_scripts {
+    "shared/*.lua",
+    "server/*.lua"
+}
+
+client_scripts {
+    "shared/*.lua",
+    "client/*.lua"
+}
+
